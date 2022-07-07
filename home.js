93d74c40b46cf65b50d348adac38ed3c799b5be8
PedroContessoto1 <pedro.contessoto@pucpr.edu.br>
2022-07-07 00:05:09 -0300

    first commit

---
+++ home.js
@@ -0,0 +1,25 @@
+function s_porc(a, b) {
+    return a * (1 - b * 0.01)
+}
+
+function ruler(pcompra, pvenda, nitem){
+    var pcompra = s_porc(pcompra, 1.5)
+    let paid_value = nitem * pcompra
+    var pvenda = s_porc(pvenda, 4.5)
+    let gross_profit = nitem * pvenda
+    let net_profit = gross_profit - paid_value
+    let imposto = paid_value * 0.015
+    return [paid_value.toFixed(2), gross_profit.toFixed(2), net_profit.toFixed(2), imposto.toFixed(2)]
+}
+
+function calcule(){
+    var pcompra = document.getElementById("pcompra").value;
+    var pvenda = document.getElementById("pvenda").value;
+    var nitem = document.getElementById("nitem").value;
+    var ls_results = ruler(pcompra, pvenda, nitem)
+    document.getElementById("paid_value").value = ls_results[0];
+    document.getElementById("gross_profit").value = ls_results[1];
+    document.getElementById("net_profit").value = ls_results[2];
+    document.getElementById("imposto").value = ls_results[3];
+    console.log("chegou")
+}
